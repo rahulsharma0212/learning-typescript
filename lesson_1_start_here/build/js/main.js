426aad2b24a65d2117ed0b635c660207dba0cf72
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-08-26 11:34:08 +0530

    lesson 1 completed

---
+++ lesson_1_start_here/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = "Rahul Sharma";
+console.log("🚀 ~ file: main.ts:2 ~ username:", username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
